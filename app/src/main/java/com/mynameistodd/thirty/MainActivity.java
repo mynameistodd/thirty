803a91b28dbbbc54fdc62baa321fc2e65f7c6b08
Todd DeLand <todd.deland@gmail.com>
2014-08-29 07:14:53 -0400

    Adds dialog to show paired devices.

--- app/src/main/java/com/mynameistodd/thirty/MainActivity.java
+++ app/src/main/java/com/mynameistodd/thirty/MainActivity.java
@@ -1,23 +1,34 @@
 package com.mynameistodd.thirty;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 
 public class MainActivity extends Activity {
 
+    private Context mContext = null;
     private static final int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter mBluetoothAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this;
         setContentView(R.layout.activity_main);
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -36,6 +47,37 @@ public class MainActivity extends Activity {
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBt = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBt, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        List<String> deviceStr = new ArrayList<String>();
+        final List<String> devices = new ArrayList<String>();
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                deviceStr.add(device.getName() + "\n" + device.getAddress());
+                devices.add(device.getAddress());
+            }
+
+            ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.select_dialog_singlechoice, deviceStr);
+            AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+            dialog.setSingleChoiceItems(adapter, -1, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    //int position = ((AlertDialog)dialog).getListView().getCheckedItemPosition();
+                    //String address = devices.get(position);
+                    String address = devices.get(which);
+                    Toast.makeText(mContext, address, Toast.LENGTH_SHORT).show();
+                }
+            });
+            dialog.setTitle("Choose a paired device.");
+            dialog.show();
         }
     }
 
